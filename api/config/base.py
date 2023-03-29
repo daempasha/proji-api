7c45e0f94cc9336a8e8af99abd54669b52194282
Daem Pasha <daempasha@gmail.com>
2023-03-30 00:07:03 +0100

    Added mongodb with example route

--- api/config/base.py
+++ api/config/base.py
@@ -1,9 +1,12 @@
 """Basic configuration for all environments to inherit"""
+import os
 
 
 class Base:
     """Base configuration class"""
 
+    DEBUG = False
     FRONTEND_HOST = "http://localhost:5173"
     AUTH0_API_KEY = "API_KEY"
-    DEBUG = False
+
+    MONGO_URI = os.environ["MONGO_URI"]
