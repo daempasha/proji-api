75e5678777ca0d4a09c3f3343ae59395d8b895f1
Daem Pasha <daempasha@gmail.com>
2023-04-05 00:52:03 +0100

    Added GET and POST for boards

--- api/routes/boards/main.py
+++ api/routes/boards/main.py
@@ -1,16 +1,28 @@
 """Get boards for users."""
 
-from flask import Blueprint, Response
+from flask import Blueprint, Response, request
 from bson import json_util
 from api import mongo
+from api.auth import require_auth
 
 blueprint = Blueprint("boards", __name__, url_prefix="/api/boards")
 
 
-@blueprint.route("")
-def get_boards():
-    """Get boards from Mongodb."""
-    return Response(
-        json_util.dumps({"data": list(mongo.db.boards.find())}),
-        mimetype="application/json",
-    )
+@blueprint.route("", methods=["GET", "POST"])
+@require_auth(None)
+def boards():
+    """Handle boards."""
+    if request.method == "GET":
+        # Get all available boards.
+        return Response(
+            json_util.dumps({"data": list(mongo.db.boards.find())}),
+            mimetype="application/json",
+        )
+
+    if request.method == "POST":
+        # Create a new board
+        body = request.get_json()
+
+        mongo.db.boards.insert_one(body)
+
+        return {"message": "Successfully added board!"}
